77e1ed347023e1f8f486898daeb2678d7c63f228
Tyler Poag <tymo.mr@gmail.com>
2016-09-13 12:09:44 -0500

    Found the Length of the rhino string
    String Length
    
    Signed-off-by: Tyler Poag <tymo.mr@gmail.com>

--- Strings/src/Strings.java
+++ Strings/src/Strings.java
@@ -6,11 +6,14 @@
     CIS 170
     @author tp0719561
  */
+import java.util.Scanner;
+ 
 public class Strings 
-{
+{   
     public static void main(String[] args) 
     {
-        
+        String animal = "rhino";
+      System.out.println(animal.length());
     }
     
 }
