eb02db3cfedf3ea7caa580bbbc8264b9ab4bb285
POAG <tp0719561@otc.edu>
2016-09-15 11:58:20 -0500

    Initial Version of 6.7 + Changes to 5_47
    Signed-off-by: POAG <tp0719561@otc.edu>

--- Assign5_47/src/Assign5_47.java
+++ Assign5_47/src/Assign5_47.java
@@ -9,7 +9,13 @@ public class Assign5_47
     public static void main(String[] args) 
     {
         
+        String isbn;
+        isbn = "1234";
+        int sum = 0;
         
+        int k = isbn.charAt(3) -48;
+        System.out.println(k);
+        sum+=k;
         
     }
     
---
+++ Assign6_7/src/Assign6_7.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author tp0719561
+ */
+public class Assign6_7 {
+
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
