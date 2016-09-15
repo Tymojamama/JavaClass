5117d956d989b14a65aba70c97b559bcb0725732
POAG <tp0719561@otc.edu>
2016-09-15 12:07:33 -0500

    Initial Version of Assign_Opening_Problem
    Signed-off-by: POAG <tp0719561@otc.edu>

---
+++ Assign_Opening_Problem/src/Assign_Opening_Problem.java
@@ -0,0 +1,36 @@
+
+
+/**
+ *
+ * @author tp0719561
+ */
+public class Assign_Opening_Problem 
+{
+
+    public static int sum(int i1, int i2)
+    {
+    int sum = 0;
+    for(int i = i1; i <=i2; i++)
+        {
+            sum +=1;
+            
+        }
+    return sum;
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        
+        
+        int sum = 0;
+       //for(int i = 1; i<= 10; i++)
+       //{
+         //  sum += 1;
+           System.out.println("Sum from 1 to 10 is "+ sum(1, 10));
+           
+           
+       //}
+    }
+    
+}
