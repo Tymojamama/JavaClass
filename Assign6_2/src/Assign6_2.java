793e425f11a5ee079a5deb18566980c84c07b62d
tymojamama <poagtyler@yahoo.com>
2016-09-17 22:24:32 -0500

    second complete build of the Assignment Assgin6_2. works at the moment and preforms the solution to the problems.

--- Assign6_2/src/Assign6_2.java
+++ Assign6_2/src/Assign6_2.java
@@ -10,24 +10,69 @@ public class Assign6_2
     
     public static int sumDigits(long n)
     {
-        int digitsCalc=0;
+       // int digitsCalc=0;
     
-        return digitsCalc;
+        long m=0;
+        
+        long o=0;
+        
+       System.out.println("The sum of the digits for " +n+" is");
+            //String IntCalc = Integer.toString(digitsCalc);
+          
+           // Long longCalc = Long.parseLong(IntCalc);
+        
+           
+        while(n !=0)
+        {
+            
+           m = n %10;
+        
+        
+        // System.out.println(n +" "+ m + " "+ o);
+         
+            n = n/10;
+        
+            o = m + o;
+        
+        }
+        
+        
+        System.out.println(o);
+         
+          // System.out.println(m );
+           
+           // System.out.println(n );
+           int IntCalc = Math.round(o);
+         
+        
+        
+              
+       // System.out.println( n + m);
+       
+        
+          return IntCalc;     
+        
+         
+        //long IntCalc = Long.parseLong(digitsCalc);
+        
+       // System.out.println(n);
     }
 
    
     public static void main(String[] args) 
     { 
-        int digit;
+        long digit;
        Scanner ScannerObject = new Scanner(System.in);
        
-       System.out.println("Please inter a number of your choice.");
+       System.out.print("Please enter a number of your choice: ");
        digit= ScannerObject.nextInt();
        
-       System.out.println(digit);
+       sumDigits(digit);
        
        
+       //System.out.println(digit);
        
+      
     }
     
 }
