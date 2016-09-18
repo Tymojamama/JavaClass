68feab4162ce2812cfdef038aeb979304dbaab27
tymojamama <poagtyler@yahoo.com>
2016-09-17 19:51:49 -0500

    first build of the Assignment Assgin6_2 initialized the scanner and laid framework for the first part of the problem.

--- Assign6_2/src/Assign6_2.java
+++ Assign6_2/src/Assign6_2.java
@@ -1,13 +1,32 @@
 /**
  *
- * @author Tyler
+ * @author tp0719561
  */
+import java.util.Scanner;
+        
 public class Assign6_2 
 {
+    
+    
+    public static int sumDigits(long n)
+    {
+        int digitsCalc=0;
+    
+        return digitsCalc;
+    }
 
    
     public static void main(String[] args) 
-    {
+    { 
+        int digit;
+       Scanner ScannerObject = new Scanner(System.in);
+       
+       System.out.println("Please inter a number of your choice.");
+       digit= ScannerObject.nextInt();
+       
+       System.out.println(digit);
+       
+       
        
     }
     
