351b09412844de65740c2b3a7f06955ef0e145ea
tymojamama <poagtyler@yahoo.com>
2016-09-21 18:34:36 -0500

    initial build of the Practice BookstoreCredit Project. DOES work at the moment and DOES preform the solution to the problems specs. ***First Build of the Project***

---
+++ BookstoreCredit/src/BookstoreCredit.java
@@ -0,0 +1,22 @@
+
+
+/**
+ *
+ * @author Tyler Poag <tp0719561@otc.edu>
+ */
+
+public class BookstoreCredit 
+{
+
+  
+    public static void main(String[] args) 
+    {
+     
+        System.out.println("Please enter your name: ");
+        
+        System.out.println("Please enter your GPA: ");
+        
+        
+    }
+    
+}
